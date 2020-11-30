0a824a25e97a2c50131dfb29c00c1101badb49e3
Bryson Palmer <brysonpalmer@gmail.com>
2020-11-30 08:17:46 -0800

    Finished logic getting data from local storage.

--- assets/script.js
+++ assets/script.js
@@ -1,195 +1,204 @@
 // Get the current moment
 var currentMoment = moment();
 
-// Get the current day, month, and number and display it to it's ID
-$("#currentDay").text(currentMoment.format("dddd, MMMM Do"));
+// Format the current moment to be day, month, and number and display it to it's ID
+$( "#currentDay" ).text( currentMoment.format( "dddd, MMMM Do" ) );
 
 // Get the current hour parsed
-var currentHour = parseInt(currentMoment.format("H"));
-console.log(currentHour); 
+var currentHour = parseInt( currentMoment.format( "H" ) );
+// Log current hour
+console.log( currentHour ); 
 
+// Global variables to update
 var currentHourId;
 var clickedHour;
 var textValue;
 
 // Set the status of the hour (past, present, or future) and load any local storage data
-for (var i = 9; i <= 18; i++) {
+for ( var i = 9; i <= 18; i++ ) {
 
+    // Assign the current hour id to currentHourId
     currentHourId = "#hour-" +i;
-    console.log(currentHourId);
-    checkingHourStatus();
+    // Log hour id
+    console.log( currentHourId );
+    // Call function that sets the status of the hour
+    settingHourStatus();
 
-    var savedTextValue = getTextValue();
-    console.log(savedTextValue);
-    $(currentHourId).children( "textarea" ).text(savedTextValue);
+    // Assign the data stored in local storage, to the current key, to savedTextValue
+    var savedTextValue = getTextValue( "Planner-hour-" +i );
+    // Log the value stored in savedTextValue
+    console.log( savedTextValue );
+    // Traverse the DOM starting at current hour id, targeting the child element (textarea), and set text content with savedTextValue
+    $( currentHourId ).children( "textarea" ).text( savedTextValue );
 };
 
-// Comparig the ID hour with the current hour
-function checkingHourStatus() {
-    //  Currently checking 9am < current hour
+// Sets the status of the hour to be past, present, or future
+function settingHourStatus() {
+    //  Comparig if the ID hour is less than the current hour
     if ( i < currentHour ) {
-        $(currentHourId).children( "textarea.text-container" ).addClass( "past" );
+        // DOM traversal to the child textarea and adding the class .past
+        $(currentHourId).children( "textarea" ).addClass( "past" );
         
-    //  Currently checking 9am == current hour
+    //  Comparig if the ID hour is equal to the current hour
     } else if ( i == currentHour ) {
-        $(currentHourId).children( "textarea.text-container" ).addClass( "present" );
+        // DOM traversal to the child textarea and adding the class .present
+        $(currentHourId).children( "textarea" ).addClass( "present" );
 
+    //  Default ID hour must be ahead of current hour and in the future
     } else {
-        $(currentHourId).children( "textarea.text-container" ).addClass( "future" );
+        // DOM traversal to the child textarea and adding the class .future
+        $(currentHourId).children( "textarea" ).addClass( "future" );
     }
 };
 
-// Gets local storage data
-function getTextValue() {
-    return JSON.parse(localStorage.getItem( "textValue" ));
+// Gets local storage data. Pass in text value of current key from loop
+function getTextValue( textValue ) {
+    // returns local storage value textValue
+    return JSON.parse( localStorage.getItem( textValue.trim() ));
 };
 
 // Sets local storage data
 function setTextValue() {
-    localStorage.setItem("Planner-hour-"+ clickedHour, JSON.stringify(textValue));
+    // Passing in prefix for hour, the clicked hour, and the text to be saved trimmed of white space
+    localStorage.setItem( "Planner-hour-"+ clickedHour, JSON.stringify( textValue.trim() ) );
 };
 
 // WHEN the user clicks the save button for a particular hour
 //  THEN I need to save the text associated with that hour.
 
-var hr9 = $("#hour9Button");
-var hr10 = $("#hour10Button");
-var hr11 = $("#hour11Button");
-var hr12 = $("#hour12Button");
-var hr13 = $("#hour13Button");
-var hr14 = $("#hour14Button");
-var hr15 = $("#hour15Button");
-var hr16 = $("#hour16Button");
-var hr17 = $("#hour17Button");
-var hr18 = $("#hour18Button");
+var hr9 = $( "#hour9Button" );
+var hr10 = $( "#hour10Button" );
+var hr11 = $( "#hour11Button" );
+var hr12 = $( "#hour12Button" );
+var hr13 = $( "#hour13Button" );
+var hr14 = $( "#hour14Button" );
+var hr15 = $( "#hour15Button" );
+var hr16 = $( "#hour16Button" );
+var hr17 = $( "#hour17Button" );
+var hr18 = $( "#hour18Button" );
 
-hr9.on("click", event => {
+hr9.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log("The clicked hour is: " +clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev("textarea").val();
-    console.log("The text value is: " +textValue);
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
-    
-});
-// hr9.off();
-
-hr10.on("click", event => {
-
-    event.preventDefault();
-    //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
-    //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
-    setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr11.on("click", event => {
+hr10.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr12.on("click", event => {
+hr11.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr13.on("click", event => {
+hr12.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr14.on("click", event => {
+hr13.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr15.on("click", event => {
+hr14.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr16.on("click", event => {
+hr15.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr17.on("click", event => {
+hr16.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
 
-hr18.on("click", event => {
+hr17.on( "click", event => {
 
-    event.preventDefault();
     //  GET the button's hour
-    clickedHour = $(event.target).data("hour");
-    console.log(clickedHour);
+    clickedHour = $( event.target ).data( "hour" );
     //  GET the value of the text from the previous element
-    textValue = $(event.target).prev().val();
-    console.log(textValue);
-    //  localStorage.setItem( "planner-hour-"+ theClickedHour, value )
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
     setTextValue();
+    //  reload page
+    location.reload();
 });
+
+hr18.on( "click", event => {
+
+    //  GET the button's hour
+    clickedHour = $( event.target ).data( "hour" );
+    //  GET the value of the text from the previous element
+    textValue = $( event.target ).prev().val();
+    //  Save to local storage
+    setTextValue();
+    //  reload page
+    location.reload();
+});
+
+// Logic doesn't work. Need to find a new way to clear the planner each day
+// if ( currentHour > 0 || currentHour < 9 ) {
+//     localStorage.clear();
+//     location.reload();
+// };
